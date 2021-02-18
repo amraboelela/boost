daa474bcc2f56a239117e4cb0ec77b1934c4cb13
Amr Aboelela <amraboelela@gmail.com>
2021-02-17 17:34:30 -0800

    Added config.hpp

--- README.md
+++ README.md
@@ -11,14 +11,11 @@ Follow the following steps:
 Download [boost_1_70_0.tar.bz2 from sourceforge](http://sourceforge.net/projects/boost/files/boost/1.70.0/boost_1_70_0.tar.bz2) then copy it to `iOSBoostFramework` directory.
 
     resources $ cd boost
-        boost $ ./xcodebuild.sh
-        boost $ ls
+        boost $ ./build.sh
+        boost $ cd build
+        build $ ls
 
-You should see the generated checksum.txt
-
-    boost $ ls ..
-
-You should see the generated boost.xcframework.zip
+You should see the generated `boost.xcframework.zip` and `checksum.txt`
 
 ## Swift Package Manager
 
---
+++ build.sh
@@ -0,0 +1,49 @@
+
+projectPath=$PWD
+#OSs=(macos ios ios)
+#archs=(x86_64 x86_64 arm64)
+OSs=(macos)
+archs=(x86_64)
+iOSBoostFrameworkPath=~/resources/iOSBoostFramework
+i=0
+libName=boost
+for arch in ${archs[@]}; do
+    frameworkPath=$projectPath/build/frameworks/${OSs[$i]}-$arch
+    if [ ! -d $frameworkPath/$libName.framework ]; then
+        cd $iOSBoostFrameworkPath
+        rm -rf build
+        make OS="${OSs[$i]}" ARCHS="$arch"
+        mkdir -p $frameworkPath
+        mv build/$libName.framework $frameworkPath
+        cp $projectPath/config.hpp $frameworkPath/boost.framework/Headers/asio/detail
+    else
+        echo directory $frameworkPath/$libName.framework already exists
+    fi
+    ((i=i+1))
+done
+cd $projectPath/build
+    
+if [ ! -d $libName.xcframework ]; then
+    echo Creating $libName.xcframework
+    xcodebuildCommand="xcodebuild -create-xcframework"
+    i=0
+    for arch in ${archs[@]}; do
+        xcodebuildCommand="$xcodebuildCommand -framework frameworks/${OSs[i]}-$arch/$libName.framework"
+        ((i=i+1))
+    done
+    xcodebuildCommand="$xcodebuildCommand -output $libName.xcframework"
+    eval "$xcodebuildCommand"
+else
+    echo directory $libName.xcframework already exists
+fi
+
+currentDay=$(date "+%y-%m-%d")
+mkdir -p ~/archive/$libName
+cp -p checksum.txt ~/archive/$libName/checksum-$currentDay.txt
+cp -p $libName.xcframework.zip ~/archive/$libName/$libName-$currentDay.xcframework.zip
+rm $libName.xcframework.zip
+
+zip -r $libName.xcframework.zip $libName.xcframework
+rm -rf $libName.xcframework
+
+swift package compute-checksum boost.xcframework.zip > checksum.txt
